7350e71ef9731e66345301e5d51e8a46b8a618d4
Mahmoud Khashaba <mahmoud.h.khashaba@gmail.com>
2025-08-21 00:37:33 +0300

    Update LocalizationContext.tsx

--- src/app/contexts/LocalizationContext.tsx
+++ src/app/contexts/LocalizationContext.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
-import { config } from '../../env.config';
+import { config } from '../../../env.config';
 
 export interface LocaleConfig {
     code: string;
